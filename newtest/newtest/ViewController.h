8ac783bc81c354c03182dc1d5cb7cc3ee291ad5a
贾海亮 <13220173007@163.com>
2016-06-13 09:45:29 +0800

    我改了一些东西

---
+++ newtest/newtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newtest
+//
+//  Created by 贾海亮 on 16/6/13.
+//  Copyright © 2016年 贾海亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ newtest/newtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  newtest
+//
+//  Created by 贾海亮 on 16/6/13.
+//  Copyright © 2016年 贾海亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
